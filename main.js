f681fc2b6537b72bdd62bbe61a39ea246b63788d
Arnav-1409 <44052315+Arnav-1409@users.noreply.github.com>
2018-11-05 16:32:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,78 +1,39 @@
 $(document).ready(function () {
     var output = '';
-    var URL = 'books.json';
+    var D;
     console.log("hello");
     $.ajax({
         type: 'GET',
         dataType: 'json',
         url: 'books.json',
         success: function (data) {
+            D = data.book;
             var user = data.category;
+            console.log(user);
             for (var i = 0; i < user.length; i++) {
-                output += "<li id='All'>" + user[i] + "</li>";
+                output += "<li id=" + user[i] + ">" + user[i] + "</li>";
             }
             $('#List').html(output);
         },
 
     });
-});
-
-$(function () {
-    function loadAll() {
-        var output = '';
-        var URL = 'books.json';
-        $.ajax({
-            type: 'GET',
-            dataType: 'json',
-            url: 'books.json',
-            success: function (data) {
-                var user = data.book;
-                for (var i = 0; i < user.length; i++) {
-                    output += "<div class='row2'>";
-                    output += "<div class='column2'>" + user[i].name + "</div>";
-                    output += "<div class = 'column2'>" + user[i].description + "</div>";
-                    output += "<div class='column2'>" + user[i].category + "</div>";
-                    output += "</div>";
-                }
-                $('#content').html(output);
-            },
-        });
-    }
     $('#List').on('click', function (event) {
         var user = event.target.id;
-        var i = [];
-        if ( user === user[i]) {
-            user = '';
-            loadAll();
-            console.log("if");
+        var out = "";
+        for (var i = 0; i < D.length; i++) {
+            if (user === "All" || D[i].category === user) {
+                out += "<div class='row2'>";
+                out += "<div class='column2'>" + D[i].name + "</div>";
+                out += "<div class = 'column2'>" + D[i].description + "</div>";
+                out += "<div class='column2'>" + D[i].category + "</div>";
+                out += "</div>";
+                console.log("in for loop");
+            }
         }
-        else {
-            console.log("else")
-            var output = ' ';
-            var URL = 'books.json';
-            $.ajax({
-                type: 'GET',
-                dataType: 'json',
-                url: 'books.json',
-                success: function (data) {
-                    var up_user = data.book;
-                    for (var i = 0; i < up_user.length; i++) {
-                        // console.log(data.book[i]);
-                        // console.log(up_user[i]);
-                        if (up_user[i] === category[i]) {
-                            output += "<div class='row2'>";
-                            output += "<div class='column2'>" + up_user[i].name + "</div>";
-                            output += "<div class = 'column2'>" + up_user[i].description + "</div>";
-                            output += "<div class='column2'>" + up_user[i].category + "</div>";
-                            output += "</div>";
-                            console.log("else for loop");
-                        }
-                    }
-                    $('#content').html(output);
-                    console.log("output");
-                },
-            });
-        }
-    });
+        $('#content').html(out);
+    }
+    )
 });
+
+
 
